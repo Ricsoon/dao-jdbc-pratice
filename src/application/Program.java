b718df77bd14b8203d72fb213d4f3d07a0764acd
Ricardo Vinicius <painonex2@gmail.com>
2021-10-01 14:17:20 -0300

    Busca por Departamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -10,13 +10,20 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("=== Teste 1: Seller por Id ===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+
+		System.out.println("\n=== Teste 2: Seller por Departamento ===");
+		Department dep = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
