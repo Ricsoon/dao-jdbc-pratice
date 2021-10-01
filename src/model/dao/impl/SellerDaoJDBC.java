8371a84bee0916fb4530ca2da03a2933409d29d7
Ricardo Vinicius <painonex2@gmail.com>
2021-10-01 13:17:48 -0300

    Fabrica de Dao e Classe JDBC

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void inser(Seller obj) {
+		
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		
+		
+	}
+
+	@Override
+	public void deleteById(Integer Id) {
+		
+		
+	}
+
+	@Override
+	public Seller findById(Integer Id) {
+		
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		
+		return null;
+	}
+
+}
